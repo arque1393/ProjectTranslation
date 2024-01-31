152460109ea20363d823b65dc806acac5c1e6e2a
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2024-01-31 16:33:22 +0530

    update

---
+++ WebService/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+
+model = MBartForConditionalGeneration.from_pretrained("SnypzZz/Llama2-13b-Language-translate")
+tokenizer = MBart50TokenizerFast.from_pretrained("SnypzZz/Llama2-13b-Language-translate", src_lang="en_XX")
+
+
+def translate_en_to_gu(article_en : str = ""):
+    try:
+        model_inputs = tokenizer(article_en, return_tensors="pt")
+        generated_tokens = model.generate(
+            **model_inputs,
+            forced_bos_token_id=tokenizer.lang_code_to_id["gu_IN"]
+        )
+        return tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    except Exception as e :
+        print(e)
+    
+
+
+
+
+app = FastAPI()
+
+@app.post("/api/translate")
+def translator_api(article:str):
+    try: 
+        article_gu = translate_en_to_gu(article)
+        return {"article_gu":article_gu}
+    except Exception as e: 
+        raise HTTPException(e)
